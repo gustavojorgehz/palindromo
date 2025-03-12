9e2c42ebedc508fc2e78fd67300965cbbfc1422f
Gustavo Jorge <gustavojorgehz@gmail.com>
2025-03-12 16:39:00 -0300

    Add files to creat the project
    Add the html sample

---
+++ palindramar/assets/main.js
@@ -0,0 +1,17 @@
+let text = document.querySelector("#text-input");
+let bnt = document.querySelector("#check-bnt");
+let resu = document.querySelector("#result");
+let ara = document.querySelector("#po")
+
+// Verify void
+    function verify(palindromo) {
+
+    }
+
+    verify()
+
+    if(text == "") {
+        bnt.addEventListener("click", ()=>{
+            window.alert("Please input a value");
+        })
+    }
