0ec38050272a37d716016d9b4792331c0fb3734a
Gustavo Jorge <gustavojorgehz@gmail.com>
2025-04-09 15:38:54 -0300

    Update main.js

--- palindramar/assets/main.js
+++ palindramar/assets/main.js
@@ -1,17 +1,40 @@
-let text = document.querySelector("#text-input");
-let bnt = document.querySelector("#check-bnt");
-let resu = document.querySelector("#result");
-let ara = document.querySelector("#po")
+const userInput = document.getElementById('text-input');
+const checkPalindromeBtn = document.getElementById('check-btn');
+const resultDiv = document.getElementById('result');
 
-// Verify void
-    function verify(palindromo) {
+const checkForPalindrome = input => {
+  const originalInput = input; // Store for later output
 
-    }
+  if (input === '') {
+    alert('Please input a value');
+    return;
+  }
 
-    verify()
+  // Remove the previous result
+  resultDiv.replaceChildren();
 
-    if(text == "") {
-        bnt.addEventListener("click", ()=>{
-            window.alert("Please input a value");
-        })
-    }
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+  let resultMsg = `${originalInput} ${
+    lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+  } a palindrome.`;
+
+  const pTag = document.createElement('p');
+  pTag.className = 'user-input';
+  pTag.innerText = resultMsg;
+  resultDiv.appendChild(pTag);
+
+  // Show the result.
+  resultDiv.classList.remove('hidden');
+};
+
+checkPalindromeBtn.addEventListener('click', () => {
+  checkForPalindrome(userInput.value);
+  userInput.value = '';
+});
+
+userInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    checkForPalindrome(userInput.value);
+    userInput.value = '';
+  }
+});
